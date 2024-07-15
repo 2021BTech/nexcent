1a6ae123db8671032a29877781723d96a8afe65c
daniel.becon <daniel.becon@premiumtrustbank.com>
2024-07-15 23:29:03 +0100

    blog section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/Navbar";
 import AboutSection from "./pages/About_Section/AboutSection";
+import BlogSection from "./pages/Blog_Section/BlogSection";
 import ClientSection from "./pages/Client_Section/ClientSection";
 import Features from "./pages/Features_Section/Features";
 import Footer from "./pages/Footer_Section/Footer";
@@ -14,6 +15,7 @@ function App() {
         <ClientSection />
         <AboutSection />
         <Features />
+        <BlogSection />
         <Footer />
       </div>
     </div>
---
+++ src/pages/Blog_Section/BlogSection.jsx
@@ -0,0 +1,93 @@
+import blog1 from "../../assets/blog1.svg";
+import blog2 from "../../assets/blog2.svg";
+import blog3 from "../../assets/blog3.svg";
+
+const BlogSection = () => {
+  return (
+    <section className="bg-white py-8">
+      <div className="container mx-auto">
+        <h1 className="text-[32px] text-center text-darkGrey font-bold mb-8">
+          Caring is the new marketing
+          <p className="text-lightGrey text-[14px] font-light">
+            The Nexcent blog is the best place to read about the latest
+            membership insights,
+            <br /> trends and more. See who&lsquo;s joining the community, read
+            about how our community
+            <br /> are increasing their membership income and lot&lsquo;s more.
+          </p>
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {/* Blog 1 */}
+          <div className="relative">
+            <img
+              src={blog1}
+              alt="Blog 1"
+              className="w-full h-auto rounded-lg"
+            />
+            <div className="absolute -bottom-14 left-0 right-0 p-6 m-4 bg-gray-100 bg-opacity-100 rounded-lg ">
+              <h2 className="text-xl text-darkGrey font-bold mb-2">
+                Creating Streamlined Safeguarding Processes with OneRen
+              </h2>
+              <a
+                href="#"
+                className="text-mainColorShadeOne font-bold flex items-center"
+              >
+                Read more
+                <span className="ml-2">&rarr;</span>
+              </a>
+            </div>
+          </div>
+
+          {/* Blog 2 */}
+          <div className="relative">
+            <img
+              src={blog2}
+              alt="Blog 1"
+              className="w-full h-auto rounded-lg"
+            />
+            <div className="absolute -bottom-14 left-0 right-0 p-6 m-4 bg-gray-100 bg-opacity-100 rounded-lg">
+              <h2 className="text-xl text-darkGrey font-bold mb-2">
+                What are your safeguarding responsibilities and how can you
+                manage them?
+              </h2>
+
+              <a
+                href="#"
+                className="text-mainColorShadeOne font-bold flex items-center"
+              >
+                Read more
+                <span className="ml-2">&rarr;</span>
+              </a>
+            </div>
+          </div>
+
+          {/* Blog 3 */}
+          <div className="relative">
+            <img
+              src={blog3}
+              alt="Blog 1"
+              className="w-full h-auto rounded-lg"
+            />
+            <div className="absolute -bottom-14 left-0 right-0 p-6 m-4 bg-gray-100 bg-opacity-100 rounded-lg">
+              <h2 className="text-xl text-darkGrey font-bold mb-2">
+                Revamping the Membership Model with Triathlon Australia and its
+                coast
+              </h2>
+
+              <a
+                href="#"
+                className="text-mainColorShadeOne font-bold flex items-center"
+              >
+                Read more
+                <span className="ml-2">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlogSection;
